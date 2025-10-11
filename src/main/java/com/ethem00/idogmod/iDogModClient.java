1b7f1448e7fd9dfb9cb9be17280d1f7902f75440
Ethem <ficiciethem@gmail.com>
2025-10-11 05:10:41 +0300

    Initial commit for the CurseForge 2025 ModJam of the Future 2025

---
+++ src/main/java/com/ethem00/idogmod/iDogModClient.java
@@ -0,0 +1,10 @@
+package com.ethem00.idogmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class iDogModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/ethem00/idogmod/iDogModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ethem00.idogmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class iDogModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
